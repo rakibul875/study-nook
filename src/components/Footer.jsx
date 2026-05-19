d53633ec5b009d661547b683291195361ee3e0d8
rakibul875 <rakibulislamashik78@gmail.com>
2026-05-20 01:21:00 +0600

    add footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React from "react";
+import { CiShare2 } from "react-icons/ci";
+import { FaMailBulk } from "react-icons/fa";
+import {
+  FaCamera,
+  FaEarthAsia,
+  FaLocationArrow,
+  FaPhone,
+} from "react-icons/fa6";
 
 const Footer = () => {
-    return (
-        <div>
-            Footer
-        </div>
-    );
+  return (
+    <div>
+      <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+        <nav>
+          <aside>
+            <h1 className="text-cyan-700 text-xl">StudyNook</h1>
+            <p className="text-lg">
+              © 2026 StudyNook. Focused flow <br/> for modern learners.
+            </p>
+            <div className="flex items-center gap-2 text-xl mt-3">
+              <CiShare2 />
+              <FaEarthAsia />
+              <FaCamera />
+            </div>
+            
+          </aside>
+        </nav>
+        <nav>
+          <h6 className="footer-title text-cyan-700">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Help Center</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title text-cyan-700">Legal</h6>
+          <a className="link link-hover">Terms of Service</a>
+          <a className="link link-hover">Privacy policy</a>
+        </nav>
+        <form>
+          <h6 className="footer-title text-cyan-700">Newsletter</h6>
+          <fieldset className="w-80">
+            <label>Get study tips and room updates.</label>
+            <div className="join flex flex-col gap-2">
+              <input
+                type="text"
+                placeholder="username@gmail.com"
+                className="input input-bordered join-item"
+              />
+              <button className="btn bg-cyan-700 join-item">Subscribe</button>
+            </div>
+          </fieldset>
+        </form>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,9 @@ import { useRouter } from "next/navigation";
 
 const NavBar = () => {
   const router = useRouter();
-  const handelSgnOut= async()=>{
-    router.push('/')
-  }
-
+  const handelSgnOut = async () => {
+    router.push("/");
+  };
 
   const links = (
     <>
@@ -75,7 +74,7 @@ const NavBar = () => {
         <div className="navbar-end gap-2">
           <div className="navbar-end gap-2">
             {/* {user ? ( */}
-              {/* <div className="flex gap-3">
+            {/* <div className="flex gap-3">
                 <Avatar>
                   <Avatar.Image
                     alt="John Doe"
@@ -87,17 +86,66 @@ const NavBar = () => {
                 <button onClick={handelSgnOut} className="btn bg-orange-500 text-white">Logout</button>
               </div> */}
             {/* ) : ( */}
-              <div className="flex gap-1 lg:gap-3">
-                <Link
-                  href="/login"
-                  className="btn border border-cyan-700-500 text-cyan-700"
+            <div className="">
+              <label className="toggle text-base-content">
+                <input
+                  type="checkbox"
+                  value="synthwave"
+                  className="theme-controller"
+                />
+
+                <svg
+                  aria-label="sun"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
                 >
-                  Login
-                </Link>
-                <Link href="/sineUp" className="btn bg-cyan-700 text-white">
-                  Register
-                </Link>
-              </div>
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <circle cx="12" cy="12" r="4"></circle>
+                    <path d="M12 2v2"></path>
+                    <path d="M12 20v2"></path>
+                    <path d="m4.93 4.93 1.41 1.41"></path>
+                    <path d="m17.66 17.66 1.41 1.41"></path>
+                    <path d="M2 12h2"></path>
+                    <path d="M20 12h2"></path>
+                    <path d="m6.34 17.66-1.41 1.41"></path>
+                    <path d="m19.07 4.93-1.41 1.41"></path>
+                  </g>
+                </svg>
+
+                <svg
+                  aria-label="moon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
+                  </g>
+                </svg>
+              </label>
+            </div>
+            <div className="flex gap-1 lg:gap-3">
+              <Link
+                href="/login"
+                className="btn border border-cyan-700-500 text-cyan-700"
+              >
+                Login
+              </Link>
+              <Link href="/sineUp" className="btn bg-cyan-700 text-white">
+                Register
+              </Link>
+            </div>
             {/* )} */}
           </div>
         </div>
