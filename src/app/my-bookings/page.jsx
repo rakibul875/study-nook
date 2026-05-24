4abd6cafaad45104485db1a1be4cec48dec9efc0
rakibul875 <rakibulislamashik78@gmail.com>
2026-05-24 09:11:00 +0600

    myListingsPage for improved layout and data handling; add NoDataFound component for empty state

--- src/app/my-bookings/page.jsx
+++ src/app/my-bookings/page.jsx
@@ -15,46 +15,53 @@ const myBookingsPage = async () => {
 
   const res = await fetch(`http://localhost:8000/bookings/${userId}`);
   const userBookingData = await res.json();
+  console.log(userBookingData);
+  const userDataLength = userBookingData.length;
 
   return (
     <div className="container mx-auto mt-10">
-      <div className="flex justify-between">
+      {userDataLength > 0 ? (
         <div className="">
-          <h1 className="text-4xl font-bold text-gray-500">My Bookings</h1>
-          <p className="text-lg text-gray-400">
-            Manage your upcoming and past study sessions.
-          </p>
-        </div>
-        <div className="flex gap-3">
-          <button className="btn btn-soft rounded-full">
-            <AiOutlineAlignCenter />
-            Filter
-          </button>
-          <button className="btn btn-soft rounded-full">
-            <FaDownload /> Export
-          </button>
-        </div>
-      </div>
-      <div className="my-10">
-        <Table variant="secondary">
-          <Table.ScrollContainer>
-            <Table.Content aria-label="Team members" className="min-w-[600px]">
-              <Table.Header>
-                <Table.Column isRowHeader>Room Name</Table.Column>
-                <Table.Column>Booking Date</Table.Column>
-                <Table.Column>Booking Time</Table.Column>
-                <Table.Column>Status</Table.Column>
-                <Table.Column>Action</Table.Column>
-              </Table.Header>
-              <Table.Body>
-                {userBookingData.map((userBooking) => (
-                  <Table.Row key={userBooking._id}>
-                    <Table.Cell>{userBooking.roomName}</Table.Cell>
-                    <Table.Cell>{userBooking.date}</Table.Cell>
-                    <Table.Cell>{userBooking.timeSlot}</Table.Cell>
-                    <Table.Cell>
-                      <span
-                        className={`px-3 py-1 rounded-full text-sm font-medium
+          <div className="flex justify-between">
+            <div className="">
+              <h1 className="text-4xl font-bold text-gray-500">My Bookings</h1>
+              <p className="text-lg text-gray-400">
+                Manage your upcoming and past study sessions.
+              </p>
+            </div>
+            <div className="flex gap-3">
+              <button className="btn btn-soft rounded-full">
+                <AiOutlineAlignCenter />
+                Filter
+              </button>
+              <button className="btn btn-soft rounded-full">
+                <FaDownload /> Export
+              </button>
+            </div>
+          </div>
+          <div className="my-10">
+            <Table variant="secondary">
+              <Table.ScrollContainer>
+                <Table.Content
+                  aria-label="Team members"
+                  className="min-w-[600px]"
+                >
+                  <Table.Header>
+                    <Table.Column isRowHeader>Room Name</Table.Column>
+                    <Table.Column>Booking Date</Table.Column>
+                    <Table.Column>Booking Time</Table.Column>
+                    <Table.Column>Status</Table.Column>
+                    <Table.Column>Action</Table.Column>
+                  </Table.Header>
+                  <Table.Body>
+                    {userBookingData.map((userBooking) => (
+                      <Table.Row key={userBooking._id}>
+                        <Table.Cell>{userBooking.roomName}</Table.Cell>
+                        <Table.Cell>{userBooking.date}</Table.Cell>
+                        <Table.Cell>{userBooking.timeSlot}</Table.Cell>
+                        <Table.Cell>
+                          <span
+                            className={`px-3 py-1 rounded-full text-sm font-medium
                                 ${
                                   userBooking.status === "confirmed"
                                     ? "bg-green-300 text-green-600"
@@ -63,21 +70,26 @@ const myBookingsPage = async () => {
                                       : "bg-gray-100 text-gray-500"
                                 }
                                `}
-                      >
-                        {userBooking.status}
-                      </span>
-                    </Table.Cell>
-                    <Table.Cell>
-                      <Button variant="outline">Cancel</Button>
-                    </Table.Cell>
-                  </Table.Row>
-                ))}
-              </Table.Body>
-            </Table.Content>
-          </Table.ScrollContainer>
-        </Table>
-        <NoDataFound />
-      </div>
+                          >
+                            {userBooking.status}
+                          </span>
+                        </Table.Cell>
+                        <Table.Cell>
+                          <Button variant="outline">Cancel</Button>
+                        </Table.Cell>
+                      </Table.Row>
+                    ))}
+                  </Table.Body>
+                </Table.Content>
+              </Table.ScrollContainer>
+            </Table>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <NoDataFound />
+        </div>
+      )}
     </div>
   );
 };
--- src/app/my-listings/page.jsx
+++ src/app/my-listings/page.jsx
@@ -1,29 +1,37 @@
-import RoomCard from '@/components/RoomCard';
-import { auth } from '@/lib/auth';
-import { headers } from 'next/headers';
-import React from 'react';
+import NoDataFound from "@/components/NoDataFound";
+import RoomCard from "@/components/RoomCard";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import React from "react";
 
 const myListingsPage = async () => {
-    const session = await auth.api.getSession({
-    headers: await headers(), 
-  })
-  const user=session?.user
-  const currentUserId=user?.id
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  const user = session?.user;
+  const currentUserId = user?.id;
 
-    const res= await fetch(`http://localhost:8000/my-rooms/${currentUserId}`)
-    const rooms=await res.json()
-    const userId=rooms.userId
-    const currentUser=userId===currentUserId
-    
-    return (
-        <div className='container mx-auto mt-10'>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-                {
-                    rooms.map(room=><RoomCard key={room._id} room={room}/>)
-                }
-            </div>
+  const res = await fetch(`http://localhost:8000/my-rooms/${currentUserId}`);
+  const rooms = await res.json();
+  const userId = rooms.userId;
+  const currentUser = userId === currentUserId;
+  const roomsLength = rooms.length;
+  console.log(roomsLength);
+  return (
+    <div className="container mx-auto mt-10">
+      {roomsLength > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {rooms.map((room) => (
+            <RoomCard key={room._id} room={room} />
+          ))}
         </div>
-    );
+      ) : (
+        <div>
+          <NoDataFound />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default myListingsPage;
