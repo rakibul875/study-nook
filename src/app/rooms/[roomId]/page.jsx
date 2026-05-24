d1c7feb5ee18c3d91fc92763aea3d58319316bdb
rakibul875 <rakibulislamashik78@gmail.com>
2026-05-25 02:18:55 +0600

    refactor: clean up formatting and improve code readability in RoomsDetails component

--- src/app/rooms/[roomId]/page.jsx
+++ src/app/rooms/[roomId]/page.jsx
@@ -1,36 +1,33 @@
-import BookNowCard from '@/components/BookNowCard';
-import RoomsDetailsPage from '@/components/RoomsDetailsPage';
-import { auth } from '@/lib/auth';
-import { headers } from 'next/headers';
-import React from 'react';
+import BookNowCard from "@/components/BookNowCard";
+import RoomsDetailsPage from "@/components/RoomsDetailsPage";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import React from "react";
 
 export const metadata = {
   title: "Study Nook - Room Details",
   description: "Beast Study Rooms",
 };
 
-const RoomsDetails = async({params}) => {
-    const {roomId}=await params;
-    const {token}= await auth.api.getToken({
-        headers: await headers()
-    })
+const RoomsDetails = async ({ params }) => {
+  const { roomId } = await params;
+  const { token } = await auth.api.getToken({
+    headers: await headers(),
+  });
 
-    console.log(token)
-
-    const res=await fetch(`http://localhost:8000/rooms/${roomId}`,{
-        headers:{
-            authorization:`bearer ${token}`
-        }
-    })
-    const data=await res.json()
-    return (
-        <div className=''>
-            <div className="">
-                <RoomsDetailsPage data={data}/>
-               
-            </div>
-        </div>
-    );
+  const res = await fetch(`http://localhost:8000/rooms/${roomId}`, {
+    headers: {
+      authorization: `bearer ${token}`,
+    },
+  });
+  const data = await res.json();
+  return (
+    <div className="">
+      <div className="">
+        <RoomsDetailsPage data={data} />
+      </div>
+    </div>
+  );
 };
 
 export default RoomsDetails;
